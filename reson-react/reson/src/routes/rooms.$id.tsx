c5d684c97d69f04d6de3eec7b24163a232f00c7c
sujang958 <enter7377@naver.com>
2025-09-09 11:18:37 +0900

    feat: add

--- reson-react/reson/src/routes/rooms.$id.tsx
+++ reson-react/reson/src/routes/rooms.$id.tsx
@@ -3,7 +3,8 @@ import { useEffect, useRef } from "react"
 import LiteYoutubeEmbed from "react-lite-youtube-embed"
 import YouTubeIFrameCtrl from "youtube-iframe-ctrl"
 import Button from "../components/Button"
-import { Volume2 } from "lucide-react"
+import { SkipForward, Volume2 } from "lucide-react"
+import QueueItem from "../components/QueueItem"
 
 export const Route = createFileRoute("/rooms/$id")({
   component: RouteComponent,
@@ -37,12 +38,13 @@ function RouteComponent() {
       <section>
         <p className="text-sm text-neutral-600 mt-2 text-wrap max-w-sm">{id}</p>
         <p className="mt-1 text-3xl font-semibold">Title</p>
-        <p className="text-lg text-neutral-600 mt-3 text-wrap max-w-sm">
+        {/* <p className="text-lg text-neutral-600 mt-3 text-wrap max-w-sm">
           lets watch together
-        </p>
+        </p> */}
       </section>
-      <section className="flex flex-col flex-1">
-        <aside>
+
+      <section className="flex flex-wrap flex-row items-start justify-between gap-12 mt-8">
+        <aside className="shrink">
           <LiteYoutubeEmbed
             id="xvFZjo5PgG0"
             title="d"
@@ -52,14 +54,26 @@ function RouteComponent() {
             autoplay
             muted
           />
-          <div className="flex flex-row items-center mt-3">
-            <Button>
+          <div className="flex flex-row items-center mt-3 gap-x-2">
+            <Button onClick={() => ytCtrl.current?.unMute()}>
               <Volume2 size={16} />
               Unmute
             </Button>
+            <Button onClick={() => ytCtrl.current?.unMute()}>
+              <SkipForward size={16} />
+              Skip
+            </Button>
           </div>
         </aside>
-        <main></main>
+        <main className="min-w-sm flex-1 bg-black rounded-lg py-3 px-4 h-[60lvh]">
+          <p className="text-2xl font-medium">Queue</p>
+          <div className="py-2 flex flex-col mt-3 gap-y-3.5">
+            <QueueItem />
+            <QueueItem />
+            <QueueItem />
+            <QueueItem />
+          </div>
+        </main>
       </section>
       <div className="py-6"></div>
     </>
